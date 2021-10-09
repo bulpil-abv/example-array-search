a415626158e6e3fd40babf05baa9019c35797538
bulpil <bulpil@abv.bg>
2021-10-10 00:44:52 +0300

    simple numbers

---
+++ example-simple-numbers.py
@@ -0,0 +1,23 @@
+def is_simple_number(x: int):
+    """
+    :parameter x is positive integer.
+    Check if x is a simple number.
+    """
+    divisor = 2
+    while divisor < x:
+        if x % divisor == 0:
+            return False
+        divisor += 1
+    return True
+
+
+def test_is_simple_number():
+    A = [2, 3, 5, 7, 11, 17, 19]
+    for i in range(7):
+        if not is_simple_number(A[i]):
+            print("test-fail")
+            break
+    print("test-ok")
+
+
+test_is_simple_number()
