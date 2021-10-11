bb959f494a8aae438ba49002b3b1e0bb36742d56
bulpil <bulpil@abv.bg>
2021-10-11 09:07:15 +0300

    list by expression demo

---
+++ example-sorting.py
@@ -0,0 +1,24 @@
+#quadratic sorts N-size array -> O(N**2)
+
+
+def example_insert_sort(A:list):
+    """
+    N-1 passes
+    :param A:
+    :return:
+    """
+
+
+def example_choice_sort(A:list):
+    """
+    N-1 passes
+    :param A:
+    :return:
+    """
+
+def example_bubble_sort(A:list):
+    """
+    N*(N-1)/2 passes
+    :param A:
+    :return:
+    """
