df5e129733771543a690bfe36e66debf5329b0c8
bulpil <bulpil@abv.bg>
2021-10-13 00:37:38 +0300

    sort demo

--- example-sorting.py
+++ example-sorting.py
@@ -4,24 +4,25 @@
 def example_insert_sort(A: list):
     """
     N-1 passes
-    :param A:
-    :return:
     """
+    N = len(A)
+    for top in range(1, N):
+        k = top
+        while k > 0 and A[k - 1] > A[k]:
+            A[k], A[k - 1] = A[k - 1], A[k]
+            k -= 1
 
 
 def example_choice_sort(A: list):
     """
     N-1 passes
-    :param A:
-    :return:
     """
+    N = len(A)
 
 
 def example_bubble_sort(A: list):
     """
     N*(N-1)/2 passes
-    :param A:
-    :return:
     """
 
 
@@ -31,10 +32,10 @@ def test_sort(sort_algorithm):
     A = [4, 2, 5, 1, 3]
     A_sorted = [1, 2, 3, 4, 5]
     sort_algorithm(A)
-    print("OK" if A == A_sorted else "fail") #terniary operator
+    print("OK" if A == A_sorted else "fail")  # terniary operator
 
     print("testcase #2: ", end="")
-    A = list(range(10, 20))+list(range(0,10))
+    A = list(range(10, 20)) + list(range(0, 10))
     A_sorted = list(range(20))
     sort_algorithm(A)
     print("OK" if A == A_sorted else "fail")
@@ -44,6 +45,7 @@ def test_sort(sort_algorithm):
     A_sorted = [1, 2, 2, 4, 4]
     sort_algorithm(A)
     print("OK" if A == A_sorted else "fail")
+
 
 if __name__ == "__main__":
     test_sort(example_insert_sort)
