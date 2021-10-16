db5b33ff0ad0e1f26b43db46562e7fee037ddec0
bulpil <bulpil@abv.bg>
2021-10-16 13:29:13 +0300

    sort demo

--- example-sorting.py
+++ example-sorting.py
@@ -29,6 +29,11 @@ def example_bubble_sort(A: list):
     """
     N*(N-1)/2 passes
     """
+    N = len(A)
+    for bypass in range(1,N):
+        for k in range(0,N-bypass):
+            if A[k] > A[k+1]:
+                A[k], A[k+1] = A[k+1], A[k]
 
 
 def test_sort(sort_algorithm):
