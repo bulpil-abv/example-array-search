36c0ac7976763d9dacab48d7f58e3e5efac389f7
bulpil <bulpil@abv.bg>
2021-10-16 12:49:43 +0300

    sort demo

--- example-sorting.py
+++ example-sorting.py
@@ -18,6 +18,11 @@ def example_choice_sort(A: list):
     N-1 passes
     """
     N = len(A)
+    for pos in range(0,N-1):
+        for k in range(pos+1,N):
+            if A[k] < A[pos]:
+                A[k],A[pos] = A[pos],A[k]
+    #print(A)
 
 
 def example_bubble_sort(A: list):
