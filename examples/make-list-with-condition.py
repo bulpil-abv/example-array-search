c9cc3eb91be4fb88b7294f69ace33a5cf51ec1f2
bulpil <bulpil@abv.bg>
2021-10-19 11:20:45 +0300

    list with condition

--- examples/make-list-with-condition.py
+++ examples/make-list-with-condition.py
@@ -9,6 +9,7 @@ def make_list_with_condition(A:list):
 def test_make_list_with_condition():
     A=list(range(1,16,1))           # one way to make a list
     A=[x for x in range(1,16)]      # another way to make a list
+    A=(x for x in range(1,16))      # make a tuple - immutable
     C=[7,14]
     print(A)
     B=make_list_with_condition(A)
