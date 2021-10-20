b43baa184d6ced98adfa61bf38bc712db99b2935
bulpil <bulpil@abv.bg>
2021-10-20 22:13:33 +0300

    class Goat

--- examples/classes-objects.py
+++ examples/classes-objects.py
@@ -2,5 +2,20 @@ class Goat:
     age = 0
     name = ""
     weight = 0.0
+
     def show(self):
         print(self.name)
+        print(self.age)
+
+
+a = Goat()
+print(type(a))
+a.name = "Jolly"
+a.age = 2
+
+b = Goat()
+print(type(b))
+b.name = "Nelly"
+b.age = 3
+
+a.show()
