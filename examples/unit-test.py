a78616fd65ac69cd145a57fe066a255c66e7c526
bulpil <bulpil@abv.bg>
2021-10-28 15:10:32 +0300

    fibonacci task started

--- examples/unit-test.py
+++ examples/unit-test.py
@@ -1,6 +1,33 @@
 import unittest
 
 
+def fib(n):
+    """
+    Calculates Fibonacci number.
+    Throws TypeError exception if called with not integer.
+    Throws ValueError exception if called with a negative or larger than contracted number.
+    :param n: integer from 0 to 9999
+    :return: integer from 0 to ...
+    """
+    pass
+
+
+class TestFibonacci(unittest.TestCase):
+
+    def test_simple(self):
+        for param, result in [(0, 0), (1, 1), (2, 1), (3, 2), (10, 55)]:
+            self.assertEqual(fib(param), result)
+
+    def test_stress(self):
+        pass
+
+    def test_negative(self):
+        pass
+
+    def test_wrong_param_type(self):
+        pass
+
+
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
