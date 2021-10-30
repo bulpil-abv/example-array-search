0c5a99a0e1cabfc1b920766917673eaa60ebb396
bulpil <bulpil@abv.bg>
2021-10-30 14:58:28 +0300

    merge sort

---
+++ examples/merge-sort.py
@@ -0,0 +1,21 @@
+def merge_sort(A):
+    """
+    Recursively breaking down problem A into
+    sub problems until solved directly.
+    The solutions are then combined.
+    :param A:
+    :return:
+    """
+
+    if len(A) <= 1:
+        return
+    middle = len(A) // 2
+    L = [A[i] for i in range(0, middle)]
+    R = [A[i] for i in range(middle, len(A))]
+    merge_sort(L)
+    merge_sort(R)
+    C = merge(L, R)
+
+
+def merge(A, B):
+    pass
