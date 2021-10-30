c95c094773b96b1bd7eb0857aac847a877d901e1
bulpil <bulpil@abv.bg>
2021-10-30 20:37:43 +0300

    basic structure created

--- examples/hoar_sort.py
+++ examples/hoar_sort.py
@@ -4,5 +4,33 @@ def hoar_sort(A):
     :param A:
     :return:
     """
-    pass
+    if len(A) <= 1:
+        return
+    barrier = A[0]
+    L = []
+    M = []
+    R = []
+    for x in A:
+        if x < barrier:
+            L.append(x)
+        elif x == barrier:
+            M.append(x)
+        else:
+            R.append(x)
+        hoar_sort(L)
+        hoar_sort(R)
+    k = 0
+    for x in L + M + R:
+        A[k] = x
+        k += 1
 
+
+def test_hoar_sort():
+    A = [5, 7, 3, -1, 0, 33, 33, 5]
+    B = [-1, 0, 3, 5, 5, 7, 33, 33]
+    C = hoar_sort(A)
+    print("Test OK" if B == C else "Test fail")
+
+
+if __name__ == "__main__":
+    test_hoar_sort()
