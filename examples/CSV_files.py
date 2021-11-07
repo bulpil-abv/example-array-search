059ee04423aafbacd74ee3876efbabd418290948
bulpil <bulpil@abv.bg>
2021-11-07 19:16:07 +0200

    initial commit

--- examples/CSV_files.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-path = "C:/Users/Smith/Desktop/output from digitizer/frame_01 70 data pts.txt"
-with open(path) as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    for line in csv_reader:
-        print(line)
