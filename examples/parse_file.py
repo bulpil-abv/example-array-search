b8f95bb6ee3a9e6852b9f17d2eff7a25b5f50038
bulpil <bulpil@abv.bg>
2021-11-08 18:18:11 +0200

    removed test prints

--- examples/parse_file.py
+++ examples/parse_file.py
@@ -25,12 +25,11 @@ def parse_file(path:str) -> list:
         dataset = []
         for i in range(4, len(lines)):
             dataset.append(lines[i].strip().split("   "))
-        print(dataset)
-        print(len(dataset))
+
         xyz = [0] * len(dataset)
 
         for i in range(len(dataset)):
             row = dataset[i]
             xyz[i] = (float(row[0]), float(row[1]), z)
-        print(xyz)
+
     return xyz
